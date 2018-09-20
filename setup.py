fa94ab7e2ffa4cc0c6b80de5c96c2ca9c6a91858
brtsanjay <sanjay.narayanan@bluerivert.com>
2018-09-20 10:26:46 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_pkg',
+    version='0.0.1',
+    description='Hello world',
+    url='http://github.com/storborg/funniest',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=['funniest'],
+    zip_safe=False
+)
